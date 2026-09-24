205b7688807ec578532b79a2decbdc737f036506
Tsvetelin Marinov <cvetelinmarinov566@gmail.com>
2026-09-24 17:29:57 +0300

    Added method UseStatusCodePagesWithReExecute() to show the default error page allways no matter the status code of the error.

--- LocalInformationSystem/Program.cs
+++ LocalInformationSystem/Program.cs
@@ -16,6 +16,7 @@ var app = builder.Build();
 if (app.Environment.IsProduction())
 {
     app.UseExceptionHandler("/Home/Error");
+    app.UseStatusCodePagesWithReExecute("/Home/Error");
 
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
