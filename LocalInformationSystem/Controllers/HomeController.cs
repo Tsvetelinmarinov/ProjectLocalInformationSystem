8d7221313a867392dd67a9938b9adb4a5e750d61
Tsvetelin Marinov <cvetelinmarinov566@gmail.com>
2026-09-24 14:18:59 +0300

    Refactored HomeController, ErrorViewModel and Program classes. No core logic chanded only some empry lines removed and changed contition of error handling in the Program.cs

--- LocalInformationSystem/Controllers/HomeController.cs
+++ LocalInformationSystem/Controllers/HomeController.cs
@@ -6,20 +6,16 @@ namespace LocalInformationSystem.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
-        {
-            return View();
-        }
+            => View();
 
         public IActionResult Privacy()
-        {
-            return View();
-        }
+            => View();
 
+        [HttpGet]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+            => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
