3056ecd1207a1ee759b6efbbac85481717e6a36f
Tsvetelin Marinov <cvetelinmarinov566@gmail.com>
2026-09-25 16:28:14 +0300

    Added auto mapper for Entity -> EntityDTO in the service layer. The mapper is still not configured.

---
+++ LocalInformationSystem.Services/Mapper/AutoMapperDTO.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace LocalInformationSystem.Services.Mapper
+{
+    /// <summary>
+    ///  Maps Entity -> ViewModel. 
+    /// </summary>
+    public class AutoMapperDTO : Profile
+    {
+    }
+}
