08d0d22ee313df257893f006085ee5b7a7b6db6b
Tsvetelin Marinov <cvetelinmarinov566@gmail.com>
2026-09-26 15:47:18 +0300

    Added MountainsController and action Index() with corresponding view in Views/Mountains. The view is empty for now.

---
+++ LocalInformationSystem/Controllers/MountainsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LocalInformationSystem.Web.Controllers
+{
+    /// <summary>
+    ///  Controller for the Mountains page.
+    /// </summary>
+    public class MountainsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
