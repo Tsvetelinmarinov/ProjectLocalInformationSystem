b3767f9f9d599da370e0c81e7959755cba899d88
Tsvetelin Marinov <cvetelinmarinov566@gmail.com>
2026-09-26 16:07:32 +0300

    Added link 'Mountains' to the main navigation bar and the action Index() in the MountainsController now is complete.

--- LocalInformationSystem/Controllers/MountainsController.cs
+++ LocalInformationSystem/Controllers/MountainsController.cs
@@ -1,16 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+
+using LocalInformationSystem.Services.ServicesInterfaces;
+using LocalInformationSystem.Web.ViewModels;
+
+using Microsoft.AspNetCore.Mvc;
+
+using static LocalInformationSystem.Web.Common.Constants;
 
 namespace LocalInformationSystem.Web.Controllers
 {
     /// <summary>
     ///  Controller for the Mountains page.
     /// </summary>
-    public class MountainsController : Controller
+    public class MountainsController(IMountainsService service, IMapper mapper) : Controller
     {
         [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var mountainDTOs = service.GetAllMountains();
+
+            var mountainModels
+                = mapper.Map<IEnumerable<MountainViewModel>>(mountainDTOs)
+                  ?? throw new InvalidOperationException(UnableToMapMountainDTOToViewModel);
+
+            return View(mountainModels);
         }
     }
 }
