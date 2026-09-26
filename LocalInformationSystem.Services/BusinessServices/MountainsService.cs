de54af7042cb343149c124a9694afeb396439e4b
Tsvetelin Marinov <cvetelinmarinov566@gmail.com>
2026-09-26 15:19:03 +0300

    Added MountainsService + inerface in the service layer. The service has one method GetAllMountains() that retrieves the Mountain entities from the repository and mapps them to MountainDTO(Service layer) via AutoMapper.

---
+++ LocalInformationSystem.Services/BusinessServices/MountainsService.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+
+using LocalInformationSystem.Data.DatabaseRepository;
+using LocalInformationSystem.Services.DataTransferObjects;
+using LocalInformationSystem.Services.ServicesInterfaces;
+
+using static LocalInformationSystem.Services.Common.Constants;
+
+namespace LocalInformationSystem.Services.BusinessServices
+{
+    /// <summary>
+    ///  Communicates with the database repository and serves the MountainsController.
+    /// </summary>
+    public class MountainsService(IRepository repository, IMapper mapper) : IMountainsService
+    {
+        /// <summary>
+        ///  Retrieves all the mountains from the database.
+        /// </summary>
+        /// <returns>
+        ///  Collection of MountainsDTO`s.
+        /// </returns>
+        public IEnumerable<MountainDTO> GetAllMountains()
+        {
+            var mountainEntities = repository.GetAllMountains();
+
+            var mountainDTOs 
+                = mapper.Map<IEnumerable<MountainDTO>>(mountainEntities) 
+                ?? throw new InvalidOperationException(UnableToMapMountainDTO);
+
+            return mountainDTOs;
+        }
+    }
+}
